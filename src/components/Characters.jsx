c20362fcf655857a8a136f5ec3a203d5901735e2
Suman Islam <sumanislam1416@gmail.com>
2023-03-15 21:31:51 +0600

    add character component

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
+import Character from './Character';
 
 export default function Characters() {
-  const fetchCharacters = async () => {
-    const response = await fetch('https://rickandmortyapi.com/api/charactr');
-    return response.json();
-  }
+  const [page, setPage] = useState(2);
 
-  const { isLoading, data } = useQuery('characters', fetchCharacters);
+  const fetchCharacters = async ({ queryKey }) => {
+		const response = await fetch(`https://rickandmortyapi.com/api/character?page=${queryKey[1]}`);
+		return response.json();
+	};
+
+  const { isLoading, data, isPreviousData } = useQuery(['characters', page], fetchCharacters, {
+    keepPreviousData: true
+  });
+
+  console.log(data);
 
   if(isLoading) {
     return <div>Loading...</div>
@@ -18,8 +25,14 @@ export default function Characters() {
   }
 
   return (
-    <div>{data.results.map(character => (
-      <div key={character.id}>{character.name}</div>
-    ))}</div>
+    <div className='characters'>
+      {data.results.map(character => (
+        <Character key={character.id} character={character} />
+      ))}
+      <div>
+        <button disabled={page === 1} onClick={() => setPage((prevPage) => prevPage -1)}>Previous</button>
+        <button disabled={isPreviousData && !data.info.next} onClick={() => setPage(prevPage => prevPage + 1)}>Next</button>
+      </div>
+    </div>
   )
 }
